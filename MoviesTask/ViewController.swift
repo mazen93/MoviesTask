9520e18084160c221ea1e67a008b4551f615a6ea
mohamed <mohamed_mazin10@hotmail.com>
2019-02-27 09:47:49 +0200

    Initial Commit

---
+++ MoviesTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesTask
+//
+//  Created by mac on 2/27/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
